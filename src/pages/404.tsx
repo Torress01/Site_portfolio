9b260ec02b2af364c8282a9f2034f71ab263e9b5
Vitor <vitorres100@outlook.com>
2025-02-04 18:17:36 -0300

    estilização

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,19 +1,25 @@
+import { Roboto } from "next/font/google";
 import Head from "next/head";
 import Link from "next/link";
+
+const roboto =  Roboto({
+  subsets: ['latin'],
+  weight: '500',
+});
 
 const E404 = () => {
     return(
         <>
         <Head>
-            <title>404 | Huriel</title>
+            <title>404 | Vitor</title>
         </Head>
-        <div>
-            <h1>404</h1>
-            <div>
-                <p>Oops, não conseguimos encontrar essa página!</p>
-                <span>Clique no botão abaixo para voltar pra Home</span>
-            </div>
-            <Link href="/">Ir para a Página Incial</Link> 
+        <div className="flex flex-col items-center text-center mt-12 md:mt-24 gap-8 md:gap-16 px-6">
+            <h1 className="text-5xl sm:text-7xl font-bold">404</h1>
+            <p className="flex flex-col gap-8 md:gap4">
+              <span>Oops, não conseguimos encontrar essa página!</span>
+              <span>Clique no botão abaixo para ser redirecionado à Página Principal</span>
+            </p>
+            <Link href="/" className={`${roboto.className} p-4 bg-v-blue-500 rounded-xl text-black mt-5 md:mt-12 w-fit `}>Ir para a Página Incial</Link> 
         </div>
         </>
     )
--- src/pages/contatos.tsx
+++ src/pages/contatos.tsx
@@ -1,18 +1,36 @@
 import Head from "next/head";
-import Link from "next/link";
 
 const Contatos = () => {
-    return(
-        <>
-            <Head>
-                <title>Contatos | Huriel</title>
-            </Head>
-            <div>
-                <h1>Contatos</h1> 
-                <Link href="/">Ir para a pagina inical</Link> 
-            </div>
-        </>
-    )   
-}
+  return(
+      <>
+        <Head>
+          <title>Contatos | Vitor</title>
+        </Head>
+        <div className="mt-12 md:mt-24 space-y-8 md:space-y-16 px-6 md:px-32">
+            <h1 className="text-5xl md:text-7xl font-bold text-center">Contatos</h1> 
+            <ul className="table mx-auto space-y-6 md:space-y-8">
+              <li className="md:text-xl">
+                <span className="font-bold">E-mail</span>
+                <div className="flex gap-1 md:gap-3 items-center">
+                  <a href="mailto:vitor.t@ges.inatel.br" className="text-sm md:text-lg text-slate-300 underline truncate">vitor.t@ges.inatel.br</a>
+                </div>
+              </li>
+              <li className="md:text-xl">
+                <span className="font-bold">Linkedin</span>
+                <div className="flex gap-1 md:gap-3 items-center">
+                  <a href="https://www.linkedin.com/in/vitorgonzaga10" className="text-sm md:text-lg text-slate-300 underline truncate">https://www.linkedin.com/in/vitorgonzaga10</a>
+                </div> 
+              </li>
+              <li className="md:text-xl">
+                <span className="font-bold">GitHub</span>
+                <div className="flex gap-1 md:gap-3 items-center">
+                  <a href="https://github.com/Torress01" className="text-sm md:text-lg text-slate-300 underline truncate">https://github.com/Torress01</a>
+                </div>
+              </li>
+            </ul> 
+        </div>
+    </>
+  );   
+};
 
 export default Contatos;
