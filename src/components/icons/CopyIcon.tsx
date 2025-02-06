2dbbd69d08f9b1bfcce8e2d1f3987072ee987dd6
Vitor <vitorres100@outlook.com>
2025-02-05 21:38:22 -0300

    menu mobile

--- src/components/icons/CopyIcon.tsx
+++ src/components/icons/CopyIcon.tsx
@@ -1,24 +1,22 @@
 import { SVGProps } from "react";
 
-interface CopyIconProps extends SVGProps<SVGSVGElement>{
+interface CopyIconProps extends SVGProps<SVGSVGElement> {
   title?: string;
 }
 
-export const CopyIcon = ({ title, ...props}: CopyIconProps) => {
+export const CopyIcon = ({ title, ...props }: CopyIconProps) => {
   return (
     <svg
       {...props}
-      stroke="currentColor"
-      fill="currentColor"
-      stroke-width="0"
-      viewBox="0 0 512 512"
+      strokeWidth="0"
+      viewBox="0 0 24 24"
       height="1em"
       width="1em"
       xmlns="http://www.w3.org/2000/svg"
     >
       {title && <title>{title}</title>}
-      <path d="M408 480H184a72 72 0 0 1-72-72V184a72 72 0 0 1 72-72h224a72 72 0 0 1 72 72v224a72 72 0 0 1-72 72z"></path>
-      <path d="M160 80h235.88A72.12 72.12 0 0 0 328 32H104a72 72 0 0 0-72 72v224a72.12 72.12 0 0 0 48 67.88V160a80 80 0 0 1 80-80z"></path>
+      <path fill="none" d="M0 0h24v24H0z"></path>
+      <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"></path>
     </svg>
   );
-}
+};
--- src/components/icons/MenuIcon.tsx
+++ src/components/icons/MenuIcon.tsx
@@ -1,18 +1,17 @@
-import { SVGProps } from 'react';
+import { SVGProps } from "react";
 
 export const MenuIcon = (props: SVGProps<SVGSVGElement>) => {
-  return(
+  return (
     <svg
       {...props}
-      stroke = "currentColor"
-      fill = "currentColor"
-      stroke-width="0"
-      viewBox = "0 0 512 512"
-      height = "1em"
-      width = "1em"
-      xmlns = "http://www.w3.org/2000/svg"
+      strokeWidth="0"
+      viewBox="0 0 24 24"
+      height="1em"
+      width="1em"
+      xmlns="http://www.w3.org/2000/svg"
     >
-      <path d="M64 384h384v-42.666H64V384zm0-106.666h384v-42.667H64v42.667zM64  128v42.665h384V128H64z"></path>
-  </svg>
+      <path fill="none" d="M0 0h24v24H0V0z"></path>
+      <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
+    </svg>
   );
 };
